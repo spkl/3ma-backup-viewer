44da8e5fd7c8255eff2aeabb142fa6d204b3fe66
spkl <seba.fischer@gmail.com>
2017-11-01 16:24:17 +0100

    add thumbnail path for video messages

--- src/3maBackupReader/Messages/VideoMessage.cs
+++ src/3maBackupReader/Messages/VideoMessage.cs
@@ -1,10 +1,22 @@
-﻿namespace LateNightStupidities.IIImaBackupReader.Messages
+﻿using System.IO;
+
+namespace LateNightStupidities.IIImaBackupReader.Messages
 {
     /// <summary>
     /// A message with an attached video.
     /// </summary>
     public class VideoMessage : MediaMessage
     {
+        /// <summary>
+        /// Gets the file name of the thumbnail in the backup folder.
+        /// </summary>
+        public string ThumbnailFileName => $"message_media_{this.Uid}";
+
+        /// <summary>
+        /// Gets the file path of the thumbnail.
+        /// </summary>
+        public string ThumbnailFilePath => Path.Combine(Path.GetDirectoryName(this.Conversation.FilePath), this.ThumbnailFileName);
+
         internal VideoMessage()
         {
         }
